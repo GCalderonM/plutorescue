71d4f6258866c48a4d7f056e7e2fecd9ac8ebf70
GalletaCM <gcalderon@10code.es>
2021-06-15 18:45:43 +0200

    Agregado cierto archivos a la carpeta public.

--- public/js/app.js
+++ public/js/app.js
@@ -54643,6 +54643,19 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/scss/announce-styles.scss":
+/*!*********************************************!*\
+  !*** ./resources/scss/announce-styles.scss ***!
+  \*********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
 /***/ "./resources/css/app.css":
 /*!*******************************!*\
   !*** ./resources/css/app.css ***!
@@ -57605,6 +57618,7 @@ process.umask = function() { return 0; };
 /******/ 			"/js/app": 0,
 /******/ 			"css/main": 0,
 /******/ 			"css/app": 0,
+/******/ 			"css/announce-styles": 0,
 /******/ 			"css/about-us": 0
 /******/ 		};
 /******/ 		
@@ -57653,10 +57667,11 @@ process.umask = function() { return 0; };
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	__webpack_require__.O(undefined, ["css/main","css/app","css/about-us"], () => (__webpack_require__("./resources/js/app.js")))
-/******/ 	__webpack_require__.O(undefined, ["css/main","css/app","css/about-us"], () => (__webpack_require__("./resources/scss/main.scss")))
-/******/ 	__webpack_require__.O(undefined, ["css/main","css/app","css/about-us"], () => (__webpack_require__("./resources/scss/about-us.scss")))
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/main","css/app","css/about-us"], () => (__webpack_require__("./resources/css/app.css")))
+/******/ 	__webpack_require__.O(undefined, ["css/main","css/app","css/announce-styles","css/about-us"], () => (__webpack_require__("./resources/js/app.js")))
+/******/ 	__webpack_require__.O(undefined, ["css/main","css/app","css/announce-styles","css/about-us"], () => (__webpack_require__("./resources/scss/main.scss")))
+/******/ 	__webpack_require__.O(undefined, ["css/main","css/app","css/announce-styles","css/about-us"], () => (__webpack_require__("./resources/scss/about-us.scss")))
+/******/ 	__webpack_require__.O(undefined, ["css/main","css/app","css/announce-styles","css/about-us"], () => (__webpack_require__("./resources/scss/announce-styles.scss")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/main","css/app","css/announce-styles","css/about-us"], () => (__webpack_require__("./resources/css/app.css")))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
